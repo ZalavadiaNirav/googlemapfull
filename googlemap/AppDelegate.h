e3a0516fdf7763fe4ffad349150e06110863716c
ZalavadiaNirav <zalavadia.nirav@cnsoftnet.com>
2017-01-18 22:20:20 +0530

    Update AppDelegate.h

--- googlemap/AppDelegate.h
+++ googlemap/AppDelegate.h
@@ -1,10 +1,7 @@
 //
 //  AppDelegate.h
 //  googlemap
-//
-//  Created by C N Soft Net on 04/01/17.
-//  Copyright © 2017 C N Soft Net. All rights reserved.
-//
+
 
 #import <UIKit/UIKit.h>
 #import <GoogleMaps/GoogleMaps.h>
